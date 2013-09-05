fa6beca6a45fd2401ae73253cb3fc348708b0c75
ekiwan <elie.kiwan@gmail.com>
2013-09-05 15:59:24 -0700

    forgot to commit. oops. almost done with basic requirements

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,23 @@
-angular.module('myApp.controllers', []);
+angular.module('myApp.controllers', [])
+
+.controller('MainController', ['$scope', '$http', function($scope, $http) {
+  $http.jsonp('http://api.reddit.com/new?jsonp=JSON_CALLBACK')
+    .success(function(data) {
+      $scope.reddits = data.data.children;
+      console.log(data.data.children);
+    })
+    
+    .error(function(data, status) {
+      console.log('great job');
+    });
+    
+  $scope.score = 0;
+
+  $scope.upVote = function() {
+    score++;
+  }
+
+  $scope.downVote = function() {
+    score--;
+  }
+}])
--- js/directives.js
+++ js/directives.js
@@ -1,1 +1,16 @@
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', [])
+
+.directive('primeDirective', function() {
+  return {
+    restrict: 'A',
+    templateUrl: 'templates/home.html',
+  
+    scope: {
+      ngModel: '='
+    }
+    
+    // link: function(scope, ele, attr) {
+    // }
+  };
+  
+});
